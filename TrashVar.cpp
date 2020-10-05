6a7afd4bac642ead4d6f1384d6dc6e4f0b8ad3b6
Fakhir Ali <ali.fakhir33@gmail.com>
2020-10-05 21:39:19 +0500

    added TrashVar program that allows you to look into unitialized values

---
+++ TrashVar.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	//bool x;
+	int x;
+	cin >> x;
+	
+	int a[x];
+	cout << a << endl;
+	for (int i = 0; i < x; i++) {
+		cout << a[i] << endl;
+	}
+
+	
+	//cout << z << endl;
+	
+	return 0;
+}
+
