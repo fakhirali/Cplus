d7fafd4b2e67fbf02d0c52400f3f4acf43c902da
Fakhir Ali <ali.fakhir33@gmail.com>
2020-10-21 14:02:19 +0000

    Added OctalHex.cpp

---
+++ OctalHex.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a = 034;//Octal
+	int b = 0xA;//HexaDecimal
+	int c = 50;//Decimal
+
+	cout << a << endl;
+	cout << b << endl;
+	cout << c << endl;
+
+	return 0;
+}
