8fcd825a06a61220772c2e06b21a7be77b84dbe5
Fakhir Ali <ali.fakhir33@gmail.com>
2020-10-21 12:59:56 +0000

    Added compareDataTypes: where float, double and int are compared

---
+++ CompareDataTypes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	
+	int a = 5;
+	double b = 5.1;
+	float c = 5.1f;
+	
+	bool d = 0;
+	string e = "hello";
+	char f = '\b';
+	
+	cout << 7/2 << endl;	
+	cout << 7.0/2 << endl;
+	cout << 7/2.0 << endl;
+
+	return 0;
+}
